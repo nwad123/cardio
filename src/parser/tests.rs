0a8aa4193b5a1295ebc1ac2743e99b1a19268995
Nick Waddoups <nick.waddoups@usu.edu>
2026-02-01 18:12:24 -0700

    Changed lex tests to macro to be more concise

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -1,75 +1,58 @@
-use crate::parser::*;
-use crate::property::*;
+use crate::parser::{lex, Token};
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{lex, Token};
 
-    #[test]
-    fn comments() {
-        let input = "// This is a comment";
-        let tokens = lex(input);
-        assert!(tokens.is_empty());
-        
-        let input = "// Multiple comments with empty lines\n\n// Comment again!\n   // Comment with leading spaces\n";
-        let tokens = lex(input);
-        assert!(tokens.is_empty());
+    macro_rules! lex_test {
+        ($($name:ident: $input:expr, $expected:expr,)*) => {
+            $( 
+                #[test]
+                fn $name() {
+                    assert_eq!(lex($input), $expected);
+                }
+            )*
+        }
+    }
+
+    lex_test! {
+        comments_empty: "// This is a comment", vec![],
+        comments_multi: "// Multiple comments with empty lines\n\n// Comment again!\n   // Comment with leading spaces\n", vec![],
+
+        boolean_true: "true", vec![Token::True],
+        boolean_false: "false", vec![Token::False],
+
+        numeric_integer: "42", vec![Token::Integer(42)],
+        numeric_float: "3.14", vec![Token::Float(3.14)],
+        numeric_float_point: ".5", vec![Token::Float(0.5)],
+        numeric_float_ten: "10.", vec![Token::Float(10.0)],
+
+        operator_arithmetic: "+ - *", vec![Token::Plus, Token::Minus, Token::Multiply],
+
+        comparison_equal: "== !=", vec![Token::Equal, Token::NotEqual],
+        comparison_rel: "< >", vec![Token::LessThan, Token::GreaterThan],
+        comparison_rel_eq: "<= >=", vec![Token::LessThanOrEqual, Token::GreaterThanOrEqual],
+        comparison_query: "=?", vec![Token::ValueQuery],
+
+        string_hello: "\"hello world\"", vec![Token::StringLabel("\"hello world\"".to_string())],
+        token_colon: ":", vec![Token::Colon],
+
+        structural_ops: "! & |", vec![Token::Not, Token::And, Token::Or],
+        structural_brackets: "( ) [ ] { }", vec![
+            Token::LParen, Token::RParen, 
+            Token::LBracket, Token::RBracket, 
+            Token::LBrace, Token::RBrace
+        ],
+        structural_comma: ",", vec![Token::Comma],
+
+        keyword_prob: "P S", vec![Token::ProbabilityQuery, Token::SteadyStateQuery],
+        keyword_path: "G X U", vec![Token::Globally, Token::Next, Token::Until],
     }
 
     #[test]
     #[should_panic]
     fn random_letters() {
         let input = "//Comment followed by letters\nabc";
-        let tokens = lex(input);
-    }
-
-    #[test]
-    fn boolean_lexing() {
-        assert_eq!(lex("true"), vec![Token::True]);
-        assert_eq!(lex("false"), vec![Token::False]);
-    }
-
-    #[test]
-    fn numeric_lexing() {
-        assert_eq!(lex("42"), vec![Token::Integer(42)]);
-        assert_eq!(lex("3.14"), vec![Token::Float(3.14)]);
-        assert_eq!(lex(".5"), vec![Token::Float(0.5)]);
-        assert_eq!(lex("10."), vec![Token::Float(10.0)]);
-    }
-
-    #[test]
-    fn operator_lexing() {
-        assert_eq!(lex("+ - *"), vec![Token::Plus, Token::Minus, Token::Multiply]);
-    }
-
-    #[test]
-    fn comparison_lexing() {
-        assert_eq!(lex("== !="), vec![Token::Equal, Token::NotEqual]);
-        assert_eq!(lex("< >"), vec![Token::LessThan, Token::GreaterThan]);
-        assert_eq!(lex("<= >="), vec![Token::LessThanOrEqual, Token::GreaterThanOrEqual]);
-        assert_eq!(lex("=?"), vec![Token::ValueQuery]);
-    }
-
-    #[test]
-    fn string_lexing() {
-         assert_eq!(lex("\"hello world\""), vec![Token::StringLabel("\"hello world\"".to_string())]);
-         assert_eq!(lex(":"), vec![Token::Colon]);
-    }
-
-    #[test]
-    fn structural_lexing() {
-         assert_eq!(lex("! & |"), vec![Token::Not, Token::And, Token::Or]);
-         assert_eq!(lex("( ) [ ] { }"), vec![
-             Token::LParen, Token::RParen, 
-             Token::LBracket, Token::RBracket, 
-             Token::LBrace, Token::RBrace
-         ]);
-         assert_eq!(lex(","), vec![Token::Comma]);
-    }
-
-    #[test]
-    fn keyword_lexing() {
-        assert_eq!(lex("P S"), vec![Token::ProbabilityQuery, Token::SteadyStateQuery]);
-        assert_eq!(lex("G X U"), vec![Token::Globally, Token::Next, Token::Until]);
+        let _tokens = lex(input);
     }
 }
